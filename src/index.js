c5708f64d583e553b4d27cc81bd7f7fe92bd3162
unknown <anastasia28-05@mail.ru>
2020-03-04 23:07:41 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
+exports.min = function min(array) {
+    if (array == undefined || array == 0) {
+        return 0;
+    } else {
+        let d = array.length;
+        let i;
+        for (i = 1; i < d; i++) {
+            if (array[i - 1] < array[i]) {
+                array[i] = array[i - 1];
+            }
+        }
+        return array[array.length - 1];
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (array == undefined || array == 0) {
+    return 0;
+} else {
+    let d = array.length;
+    let i;
+    for (i = 1; i < d; i++) {
+        if (array[i - 1] > array[i]) {
+            array[i] = array[i - 1];
+        }
+    }
+    return array[array.length - 1];
+  }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (array == undefined || array == 0) {
+    return 0;
+} else {
+    let d = array.length;
+    let i;
+    for (i = 1; i < d; i++) {
+        array[i] = array[i] + array[i - 1];
+    }
+    let s = array[array.length - 1] / d;
+    return s;
+  }
+};
